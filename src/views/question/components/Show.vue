e15a88342249270dfc11882170ce8979b2b84a8e
CNPolaris <42991837+CNPolaris@users.noreply.github.com>
2022-05-01 14:34:38 +0800

    注释fix

--- src/views/question/components/Show.vue
+++ src/views/question/components/Show.vue
@@ -9,11 +9,11 @@
           <br>
         </span>
 
-<!--        <span v-for="item in question.items" :key="item.id" class="q-item-contain">-->
-<!--          <el-tag>{{ item.prefix }}</el-tag>-->
-<!--          <span class="q-item-content" v-html="item.content" />-->
-<!--          <br>-->
-<!--        </span>-->
+        <!--        <span v-for="item in question.items" :key="item.id" class="q-item-contain">-->
+        <!--          <el-tag>{{ item.prefix }}</el-tag>-->
+        <!--          <span class="q-item-content" v-html="item.content" />-->
+        <!--          <br>-->
+        <!--        </span>-->
       </div>
     </div>
     <div v-else-if="qType===2" v-loading="qLoading">
@@ -22,24 +22,24 @@
         <span v-for="item in question.items" :key="item.id" class="q-item-contain">
           <span class="q-item-prefix">{{ item.prefix }}.</span>
           <span class="q-item-content" v-html="item.content" />
-<!--          <el-tag>{{ item.prefix }}</el-tag>-->
-<!--          <span class="q-item-content" v-html="item.content" />-->
+          <!--          <el-tag>{{ item.prefix }}</el-tag>-->
+          <!--          <span class="q-item-content" v-html="item.content" />-->
           <br>
         </span>
       </div>
     </div>
     <div v-else-if="qType===3" v-loading="qLoading">
       <div class="q-title" style="display: inline;margin-right: 10px" v-html="question.title" />
-            <span>（</span>
-            <span v-for="item in question.items" :key="item.id">
-              <span class="q-item-content" v-html="item.content" />
-            </span>
-            <span>）</span>
-      <br>
-<!--      <span v-for="item in question.items" :key="item.id">-->
-<!--        <el-tag>{{ item.prefix }}</el-tag><span class="q-item-content" v-html="item.content" />-->
-<!--        <br>-->
-<!--      </span>-->
+        <span>（</span>
+        <span v-for="item in question.items" :key="item.id">
+        <span class="q-item-content" v-html="item.content" />
+        </span>
+        <span>）</span>
+        <br>
+      <!--      <span v-for="item in question.items" :key="item.id">-->
+      <!--        <el-tag>{{ item.prefix }}</el-tag><span class="q-item-content" v-html="item.content" />-->
+      <!--        <br>-->
+      <!--      </span>-->
     </div>
     <div v-else-if="qType===4" v-loading="qLoading">
       <div class="q-title" v-html="question.title" />
