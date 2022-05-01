d3a93cbc4ff77a4d7b19920ef2d87e4636bc1072
CNPolaris <cntianxin@outlook.com>
2022-05-01 20:07:17 +0800

    Update index.js
    添加baseUrl

--- src/router/index.js
+++ src/router/index.js
@@ -403,6 +403,7 @@ export const asyncRoutes = [
 
 const createRouter = () => new Router({
   // mode: 'history', // require service support
+  base: '/teacher',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
